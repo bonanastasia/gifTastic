21f045d7fee2c9de7d3f96bd1d4c38e515ab4362
Anastasia Marie <ana.goodwin10@gmail.com>
2017-03-26 21:26:40 -0500

    updated giphy link

--- javascript/logic.js
+++ javascript/logic.js
@@ -52,7 +52,7 @@ $("#add-show").on("click", function(event){
 
  function displayGifs() {
       var searchTerm = $(this).attr("data-show");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         searchTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
 
       $.ajax({
